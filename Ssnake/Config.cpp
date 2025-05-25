ba337b2a21fb93d93599ea279870f4aa8f53beeb
VampirYT <barkavampiryt@gmail.com>
2025-05-26 00:31:42 +0300

    fix bugs

--- Ssnake/Config.cpp
+++ Ssnake/Config.cpp
@@ -110,6 +110,7 @@ void FirstWriteConfig(Config& c,game& g)
 	else
 	{
 		g.skin = "default";
+		ConfigFile << "Language=" << c.language << endl;
 		ConfigFile << "Money=" << c.money << endl;
 		ConfigFile << "Lose=" << c.Lose << endl;
 		ConfigFile << "Score=" << c.score << endl;
